55ff0afac6c56de3a87763b1f9830cab7937597a
Rajab <rkarume@gmail.com>
2022-12-04 21:53:55 +0300

    commit tree 8577f1fd6029a6a6cd99238ecf5df3ab2acfbfcd

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,26 @@
+class UsersController < ApplicationController
+    def create
+        user = User.create(user_params)
+        session[:user_id] = user.id
+        if user.valid?
+            render json: user, status: :created
+        else
+            render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
+        end
+    end
+
+    def show 
+        user = User.find_by(id: session[:user_id])
+        if user.nil?
+            render json: { error: "Not authorized" }, status: 401
+        else
+            render json: user
+        end
+    end
+
+    private
+
+    def user_params
+        params.permit(:username, :password, :password_confirmation)
+    end
+end
